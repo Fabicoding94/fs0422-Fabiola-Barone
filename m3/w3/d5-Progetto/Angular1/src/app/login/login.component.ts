931673a6ebf7622ab6d83efee7df85a394513824
Fabicoding94 <fabiolabarone.fb94@gmail.com>
2022-09-19 00:41:51 +0200

    login e sign-up fatti, contenta!

--- m3/w3/d5-Progetto/Angular1/src/app/login/login.component.ts
+++ m3/w3/d5-Progetto/Angular1/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +12,48 @@ import { FormGroup } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
 
-  form!: FormGroup;
-
-  constructor() { }
+  public loginForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+
+      email: [''],
+      password: [''],
+
+    })
   }
+
+  login(){
+
+    this.http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res=>{
+
+
+      const user = res.find((a:any)=>{
+        return a.email === this.loginForm.value.email &&
+        a.password === this.loginForm.value.password
+      }
+      );
+      if(user){
+        Swal.fire({
+          icon: 'success',
+          title: 'Login with success!',
+          showConfirmButton: false,
+          timer: 1800,
+          timerProgressBar: true
+      });
+      this.loginForm.reset();
+ this.router.navigate(['blog']);
+
+      }else{
+        alert("user not found!")}
+
+
+      }, err=> {
+        alert("Something went wrong!")
+      })
+
+      }
 
 }
--- m3/w3/d5-Progetto/Angular1/src/app/register/register.component.ts
+++ m3/w3/d5-Progetto/Angular1/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public signupForm !: FormGroup;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      fullname: [''],
+      email: [''],
+      password: [''],
+      mobile: [''],
+    })
   }
 
-}
+  signUp(){
+    this.http.post<any>("http://localhost:3000/signupUsers", this.signupForm.value)
+.subscribe(res=>{
+  Swal.fire({
+    icon: 'success',
+    title: 'Signup succesfully!',
+    showConfirmButton: false,
+    timer: 1800,
+    timerProgressBar: true
+});
+this.signupForm.reset();
+ this.router.navigate(['login']);
+
+  }, err=>{
+    alert("Something went wrong!")
+  })
+
+  }}
