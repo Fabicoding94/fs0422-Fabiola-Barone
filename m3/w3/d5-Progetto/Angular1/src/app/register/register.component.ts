e98d03acfedec03de0ffc3379703bbd2ea460ee2
Fabicoding94 <fabiolabarone.fb94@gmail.com>
2022-09-19 15:12:58 +0200

    fixato il register

--- m3/w3/d5-Progetto/Angular1/src/app/register/register.component.ts
+++ m3/w3/d5-Progetto/Angular1/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
@@ -11,19 +11,22 @@ import Swal from 'sweetalert2';
 })
 export class RegisterComponent implements OnInit {
 
+
+  isFormValid!: boolean;
   public signupForm !: FormGroup;
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private router:Router) { }
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
-      fullname: [''],
-      email: [''],
-      password: [''],
-      mobile: [''],
+      fullname: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      mobile: ['', Validators.required],
     })
   }
 
   signUp(){
+    if(this.signupForm.valid){
     this.http.post<any>("http://localhost:3000/signupUsers", this.signupForm.value)
 .subscribe(res=>{
   Swal.fire({
@@ -32,12 +35,12 @@ export class RegisterComponent implements OnInit {
     showConfirmButton: false,
     timer: 1800,
     timerProgressBar: true
-});
+})
 this.signupForm.reset();
  this.router.navigate(['login']);
 
-  }, err=>{
+})
+}else{
     alert("Something went wrong!")
-  })
-
+  }
   }}
