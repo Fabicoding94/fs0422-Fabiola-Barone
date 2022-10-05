3c74c2f37faabf5d7f0d7266fa6b674a0da9001c
Fabicoding94 <fabiolabarone.fb94@gmail.com>
2022-10-05 16:16:18 +0200

    eserciziPome_week2_day3

---
+++ backend/m1/w2/week2_day3/week2_day3_PomeEs2/ArrSom1.java
@@ -0,0 +1,23 @@
+package week2_day3_PomeEs2;
+
+public class ArrSom1 extends Thread{
+    private int[] arr;
+    public int somma;
+
+    public ArrSom1( int[] _arr){
+        arr = _arr;
+    }
+
+    public void run(){
+
+        for (int i = 0; i < arr.length; i++) {
+           somma += arr[i];
+            System.out.println(somma);
+        }
+
+        System.out.println("somma finita: " + somma);
+
+    }
+
+
+}
---
+++ backend/m1/w2/week2_day3/week2_day3_PomeEs2/package-info.java
@@ -0,0 +1,1 @@
+package week2_day3_PomeEs2;
